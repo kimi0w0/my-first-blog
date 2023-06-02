cf7c44b1eaf9980fddbff7fbd50d330fea4303bc
Kimihiro <kimihiro.nagashima@gmail.com>
2023-06-02 20:07:05 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 # views.pyはモデルとテンプレートの橋渡し
-from django.shortcuts import render,get_object_or_404 # get_list_or_404もあるので間違えないこと
+from django.shortcuts import render,get_object_or_404,redirect # get_list_or_404もあるので間違えないこと
 from .models import Post # 同じディレクトリなら.で代用できる
 from django.utils import timezone
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -13,5 +14,32 @@ def post_list(request):
 def post_detail(request, pk):
     post=get_object_or_404(Post,pk=pk)
     return render(request,'blog/post_detail.html',{'post':post})
+
+def post_new(request):
+    if request.method=="POST":
+        form=PostForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author=request.user
+            post.published_date=timezone.now()
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form=PostForm()
+    return render(request,'blog/post_edit.html',{'form':form})
+
+def post_edit(request,pk): # urlsからpkを受け取る
+    post=get_object_or_404(Post,pk=pk)
+    if request.method=="POST":
+        form=PostForm(request.POST,instance=post)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author=request.user
+            post.published_date=timezone.now()
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form=PostForm(instance=post)
+    return render(request,'blog/post_edit.html',{'form':form})
 
 
