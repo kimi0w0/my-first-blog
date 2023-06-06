72dbb9f10e1a412331482a98b7707124e3f5c1a1
Kimihiro <kimihiro.nagashima@gmail.com>
2023-06-06 20:47:55 +0900

    More1.

--- blog/views.py
+++ blog/views.py
@@ -21,7 +21,7 @@ def post_new(request):
         if form.is_valid():
             post=form.save(commit=False)
             post.author=request.user
-            post.published_date=timezone.now()
+ #           post.published_date=timezone.now()
             post.save()
             return redirect('post_detail',pk=post.pk)
     else:
@@ -31,15 +31,31 @@ def post_new(request):
 def post_edit(request,pk): # urlsからpkを受け取る
     post=get_object_or_404(Post,pk=pk)
     if request.method=="POST":
-        form=PostForm(request.POST,instance=post)
+        form=PostForm(request.POST,instance=post) # instanceは入力済の項目
         if form.is_valid():
             post=form.save(commit=False)
             post.author=request.user
-            post.published_date=timezone.now()
+ #           post.published_date=timezone.now()
             post.save()
             return redirect('post_detail',pk=post.pk)
     else:
         form=PostForm(instance=post)
     return render(request,'blog/post_edit.html',{'form':form})
+
+def post_draft_list(request):
+    posts=Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request,'blog/post_draft_list.html',{'posts':posts})
+
+def post_publish(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    post.publish()
+    return redirect('post_detail',pk=pk)
+
+def post_remove(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+
 
 
